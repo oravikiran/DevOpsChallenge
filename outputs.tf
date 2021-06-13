6af08eb2f034217e78076d8705c22624f9756436
Oruganti <rorugant@ford.com>
2021-06-13 20:34:09 +0100

    challenge1 terraform scripts;

---
+++ outputs.tf
@@ -0,0 +1,13 @@
+## Technical Challenge
+output "todoapp_url" {
+  value = "http://${oci_load_balancer.lb01.ip_addresses[0]}/todoapp/list"
+}
+
+output "bastion_public_ip" {
+  value = oci_core_instance.bastion_instance.public_ip
+}
+
+output "tomcat-server_private_ips" {
+  value = data.oci_core_vnic.tomcat-server_primaryvnic.*.private_ip_address
+}
+
